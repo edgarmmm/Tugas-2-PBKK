bdadc0c94abba5d93027f51e0a414162510544d5
edgarmmm <36809510+edgarmmm@users.noreply.github.com>
2020-02-19 16:39:45 +0700

    Add files via upload

---
+++ application/views/contact.php
@@ -0,0 +1,67 @@
+<style type="text/css">
+
+    ::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+    
+    h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+    
+    a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+    code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
+
+    #body {
+		margin: 0 15px 0 15px;
+	}
+
+    #container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+</style>
+
+<div id="container">
+    <h1>Contact Us</h1>
+    <code>
+        <div id = body>  
+            <table>
+                <tr>
+                    <td>Nama</td>
+                    <td>:</td>
+                    <td>Edgar Andrew Hutauruk</td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>:</td>
+                    <td>edgar_andrew699@yahoo.com</td>
+                </tr>
+                <tr>
+                    <td>Telp</td>
+                    <td>:</td>
+                    <td>082160313277</td>
+                </tr>
+            </table>    
+        </div>
+    </code>
+</div>
